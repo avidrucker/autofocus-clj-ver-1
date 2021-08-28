0e991a70148befce63b8693dc6158f1753a5d6fc
Avi <avidrucker@gmail.com>
2021-08-28 09:02:57 -0400

    feat basic input output

--- src/autofocus_clj_ver_1/io.clj
+++ src/autofocus_clj_ver_1/io.clj
@@ -8,3 +8,46 @@
 
 ;; (defn print-vertically [stuff-to-print]
 ;;   (concat "" (map #(str % "\n") stuff-to-print)))
+
+;; initial source template:
+;; https://github.com/ggandor/eliza-in-clojure/blob/master/src/eliza_basic.clj
+(def exit-opt " (type Q to quit)")
+(def question-x "What is your name?")
+(def response-x "Oh, your name is ")
+
+(defn isAlphaOnly? [in]
+  (=
+   (count (clojure.string/trim in))
+   (count (filter (fn [x] (Character/isLetter x)) in))))
+
+(defn valid-input?
+  [in]
+  (and
+   (not (number? in))
+   (isAlphaOnly? (str in))
+   (not= "" (clojure.string/trim (str in)))))
+
+(defn reply
+  [in]
+  (if (valid-input? in)
+    (println (str response-x (clojure.string/trim in) "!"))
+    (println "That's not valid input, try again.")))
+
+(defn not-quitting? [in]
+  (and (not= in "q") (not= in "Q")))
+
+(defn user-io
+  "Respond to user input."
+  [prompt-msg]
+  (println prompt-msg)
+  (flush)
+  (let [input (read-line)]
+    (when (not-quitting? (str input))
+      (reply input)
+      (recur prompt-msg))))
+
+(defn -main []
+  (user-io (str question-x exit-opt))
+  (println "Bye for now!"))
+
+(-main)
