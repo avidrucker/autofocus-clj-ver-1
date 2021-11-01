9ccc5e20b1d6a2d1dc6dd76ff8bd9edbd56d8af0
avidrucker <avidrucker@gmail.com>
2021-11-01 10:23:27 -0400

    update comment out unused functionality

--- src/autofocus_clj_ver_1/core.clj
+++ src/autofocus_clj_ver_1/core.clj
@@ -2,16 +2,15 @@
 (ns autofocus-clj-ver-1.core
   (:gen-class)
   (:require
-   [autofocus-clj-ver-1.cli :as cli]
+   ;; [autofocus-clj-ver-1.cli :as cli]
+   ;; [autofocus-clj-ver-1.app :as app]
 ))
 
 (defn -main
   "runs the entire AutoFocus program"
   []
-  ;; MAIN PROGRAM GOES HERE
-  ;; (io/-main)
-  ;; (demos/-main)
-  (cli/run-cli-app)
+  ;; (cli/run-cli-app app/initial-program-state)
   )
+
 
 ;; (-main)
